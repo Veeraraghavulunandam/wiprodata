fe552da6c1bbc8773f4ce5133174dc4c70bec354
Veeraraghavulunandam <nvr8285@gmail.com>
2025-07-22 07:05:29 +0530

    added a program for helloworld

---
+++ Helloworld/Helloworld/Custom.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helloworld
+{
+    internal class Custom
+    {
+        static void Main()
+        {
+            string name;
+            Console.WriteLine("Enter your Name ");
+            name = Console.ReadLine();
+            Console.WriteLine("Name is " + name);
+
+        }
+
+
+    }
+}
---
+++ Helloworld/Helloworld/calculation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helloworld
+{
+    internal class calculation
+    {
+        static void Main()
+        {
+            int a, b, c;
+            Console.WriteLine("Enter 2 Number ");
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            c = a + b;
+            Console.WriteLine("Sum is" + c);
+            c = a - b;
+            Console.WriteLine("Sub is" + c);
+            c = a * b;
+            Console.WriteLine("Mult is" + c);
+
+        }
+
+    }
+
+
+}
